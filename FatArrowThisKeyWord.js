00f66bf9cfd21216c7673785ec6ee1aec1113039
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-07-07 01:22:24 +0600

    Add files via upload

--- FatArrowThisKeyWord.js
+++ FatArrowThisKeyWord.js
@@ -1,12 +1,15 @@
 var javascript={
     name: "JavaScript",
     libraries:["React","Angular","Vue"],
-    printLibraries: function(){
-        var self=this;
-        this.libraries.forEach(function(a){
-          console.log(`${self.name} loves ${a}`)  //forEach er jonno this kaj kora er jonno self a this keyword neya hoice
+   // printLibraries: function(){
+     //var self=this;
+       // this.libraries.forEach(function(a){
+        //  console.log(`${self.name} loves ${a}`)  //forEach er jonno this kaj kora er jonno self a this keyword neya hoice
 
-        });
+       // });
+       printLibraries: function(){
+       
+        this.libraries.forEach((a)=> console.log(`${this.name} loves ${a}`)  );//fat arrow key
     },
 };
 javascript.printLibraries();
