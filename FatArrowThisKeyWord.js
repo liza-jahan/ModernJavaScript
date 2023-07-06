cf78e230fb52c74cf34e29ac99f2bdeed26d7a67
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-07-06 23:38:52 +0600

    Add files via upload

---
+++ FatArrowThisKeyWord.js
@@ -0,0 +1,12 @@
+var javascript={
+    name: "JavaScript",
+    libraries:["React","Angular","Vue"],
+    printLibraries: function(){
+        var self=this;
+        this.libraries.forEach(function(a){
+          console.log(`${self.name} loves ${a}`)  //forEach er jonno this kaj kora er jonno self a this keyword neya hoice
+
+        });
+    },
+};
+javascript.printLibraries();
