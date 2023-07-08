b8770f57fc428dc530732794c2a8ac3565015f30
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-07-08 17:18:04 +0600

    Add files via upload

---
+++ keyArray.js
@@ -0,0 +1,14 @@
+var obj={
+    name:"javaScript",
+    founder:"Brendan Eich",
+    estd:"1923"
+};
+
+var keys=Object.keys(obj);
+var values=Object.values(obj);
+var entries=Object.entries(obj);
+
+
+console.log(keys);
+console.log(values);
+console.log(entries);
