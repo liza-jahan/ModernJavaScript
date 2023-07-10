f659f4667fdbb90c26f895cf85392f4a15f2e54c
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-07-10 20:16:14 +0600

    Add files via upload

---
+++ copyWithin.js
@@ -0,0 +1,8 @@
+
+
+let a=['javaScript','java','PHP','c++','Ruby','Rust'];
+//copyWithin(target, start, end)
+
+//a.copyWithin(3,2)//end ta default vabe 6 nibe ai khan a
+a.copyWithin(-3,-2,-1)/
+console.log(a);
